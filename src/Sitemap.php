7d5a6ab5c287d1d9fde9054735661e6bc1a68c5b
Till Prochaska <mail@tillprochaska.de>
2022-04-07 11:25:11 +0200

    Ignore pages with specific templates

--- src/Sitemap.php
+++ src/Sitemap.php
@@ -17,10 +17,8 @@ class Sitemap
         $this->urlset->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
         $this->urlset->setAttribute('xmlns:xhtml', 'http://www.w3.org/1999/xhtml');
 
-        $pages = $root
-            ->index()
-            ->filterBy('intendedTemplate', '!=', 'error')
-        ;
+        $ignoreTemplates = kirby()->option('tillprochaska.localized-sitemap.ignore-templates', ['error']);
+        $pages = $root->index()->filterBy(fn ($page) => !in_array($page->intendedTemplate()->name(), $ignoreTemplates));
 
         foreach ($pages as $page) {
             $this->addPage($page);
